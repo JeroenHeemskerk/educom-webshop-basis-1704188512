06620de089dcc9fff01dfa1850f9c4d532aafd6a
Thomas van Haastrecht <thomas.hjvh@gmail.com>
2024-01-11 05:01:36 -0500

    valid registrations get added to user list

--- register.php
+++ register.php
@@ -14,8 +14,20 @@
     }
     
     function displayThanks($valsAndErrs) {
-        echo '<p>U bent geregistreerd.<p>
+        echo '<h4>Registratie gelukt!</h4>
 <div>Email: ' . $valsAndErrs['email'] . ' </div>' . PHP_EOL;
+        
+        addUser($valsAndErrs);
+    }
+    
+    function addUser($valsAndErrs, $userFile='users/users.txt') {
+        $file = fopen($userFile, 'a');
+        $email = $valsAndErrs['email'];
+        $name  = $valsAndErrs['name'];
+        $pass  = $valsAndErrs['pass'];
+        
+        $line = implode('|', array($email, $name, $pass));
+        fwrite($file, PHP_EOL . $line);
     }
     
     function displayForm($valsAndErrs) {
@@ -27,13 +39,13 @@
         //input for name
         showFormField('name', 'Naam:', 'text', $valsAndErrs);
         //input for email
-        showFormField('email', 'Email:', 'text', $valsAndErrs);
+        showFormField('email', 'Email:', 'email', $valsAndErrs);
         
         //input for password
-        showFormField('pass', 'Wachtwoord:', 'text', $valsAndErrs);
-        showFormField('passConfirm', 'Wachtwoord Herhalen:', 'text', $valsAndErrs);
+        showFormField('pass', 'Wachtwoord:', 'password', $valsAndErrs);
+        showFormField('passConfirm', 'Wachtwoord Herhalen:', 'password', $valsAndErrs);
 
-        echo '        <br>' . PHP_EOL;
+        echo '        <br><br>' . PHP_EOL;
 
         showFormEnd('Registreer');
     }
